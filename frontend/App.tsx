b264f326f4228a6e0decec0dc957721de8e9ff8f
Vlad Rindevich <vladrin@vaadin.com>
2024-03-06 12:14:09 +0200

    refactor: add auth

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,6 +1,14 @@
 import { router } from "Frontend/routes.js";
 import { RouterProvider } from "react-router-dom";
+import { configureAuth } from '@vaadin/hilla-react-auth';
 
 export default function App() {
-  return <RouterProvider router={router} />;
+  // Needs to be replaced with the real auth server request
+  const { AuthProvider, useAuth } = configureAuth(async () => ({ roles: ['admin'] }));
+
+  return (
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  );
 }
--- frontend/routes.tsx
+++ frontend/routes.tsx
@@ -1,4 +1,5 @@
 import { toReactRouter } from '@vaadin/hilla-file-router/runtime.js';
+import { protectRoutes } from '@vaadin/hilla-react-auth';
 import views from 'Frontend/generated/views.js';
 import { serverSideRoutes } from "Frontend/generated/flow/Flow";
 import {createBrowserRouter} from "react-router-dom";
@@ -6,5 +7,5 @@ import {createBrowserRouter} from "react-router-dom";
 const route = toReactRouter(views);
 route.children?.push(...serverSideRoutes);
 
-export const routes = [route]
+export const routes = protectRoutes([route]);
 export const router = createBrowserRouter(routes, {basename: new URL(document.baseURI).pathname });
